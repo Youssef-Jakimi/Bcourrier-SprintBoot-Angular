4f31dd2b6e5dd3c012f700d034278450681ffdfb
Youssef Jakimi <jakimiyoussef@gmail.com>
2025-05-04 15:53:49 +0100

    Initial commit

---
+++ src/main/java/com/courrier/Bcourrier/BcourrierApplication.java
@@ -0,0 +1,13 @@
+package com.courrier.Bcourrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcourrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcourrierApplication.class, args);
+	}
+
+}
